98f40ecc1178ad0f5cd0724edf4657039f896119
Ramy Nasser <ramynasser95@gmail.com>
2025-04-14 15:57:43 +0200

    Create TempSwiftUIView.swift (#39)

---
+++ TestGithubAction/Preview Content/TempSwiftUIView.swift
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2024 Yassir S.P.A. All rights reserved.
+//
+
+import SwiftUI
+
+struct TempSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TempSwiftUIView()
+}
