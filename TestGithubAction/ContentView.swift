d0d2d1898a71f2fabc3b55f453cb93c86cf10176
Ramy Nasser <ramynasser95@gmail.com>
2025-04-14 14:12:03 +0200

    Update ContentView.swift (#8)

--- TestGithubAction/ContentView.swift
+++ TestGithubAction/ContentView.swift
@@ -8,7 +8,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
