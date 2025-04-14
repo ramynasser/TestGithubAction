812169a1d4e5b28dabbba1a41267101ce699b12e
Ramy Nasser <ramynasser95@gmail.com>
2025-04-14 16:02:08 +0200

    Revert "Update TempSwiftUIView.swift (#42)"
    This reverts commit 9b7ecca52934a54bda69da2ee73a8ed58781f100.

--- TestGithubAction/Preview Content/TempSwiftUIView.swift
+++ TestGithubAction/Preview Content/TempSwiftUIView.swift
@@ -6,8 +6,7 @@ import SwiftUI
 
 struct TempSwiftUIView: View {
     var body: some View {
-      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-      Text("Adding release fix")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
